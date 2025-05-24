69be93045da659d0beb12ce7e041428830935996
unknown <dl.kokhtashvili@gmail.com>
2025-05-24 16:11:29 +0400

    Finish mobile header component

---
+++ src/components/MobileHeader/MobileHeader.jsx
@@ -0,0 +1,56 @@
+import styles from "./MobileHeader.module.css";
+import logo from "../../assets/logo.png";
+import signInLogo from "../../assets/signInLogo.png";
+import arrowDropDown from "../../assets/arrowDropDown.png";
+
+const MobileHeader = () => {
+  return (
+    <header className={styles.mobileHeader}>
+      <div className={styles.topRow}>
+        <div>
+          <img src={logo} alt="main logo" className={styles.mainLogo} />
+        </div>
+        <div className={styles.inputSignIn}>
+          <input className={styles.searchInput} placeholder="მოძებნე" />
+          <div>
+            <img
+              src={signInLogo}
+              className={styles.signInLogo}
+              alt="sign in logo"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.headerLine}></div>
+      <nav class="bottom-row">
+        <ul>
+          <div className={styles.categoryAndArrow}>
+            <ul>
+              <a>კატეგორიები</a>
+            </ul>
+            <img
+              className={styles.arrowDropDown}
+              src={arrowDropDown}
+              alt="arrow Drop Down"
+            />
+          </div>
+
+          <ul>
+            <a>პოლიტიკა</a>
+          </ul>
+          <ul>
+            <a>მოდა</a>
+          </ul>
+          <ul>
+            <a>სპორტი</a>
+          </ul>
+          <ul>
+            <a>სპორტი</a>
+          </ul>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MobileHeader;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import MobileHeader from "../../components/MobileHeader/MobileHeader";
+
+const HomePage = () => {
+  return (
+    <>
+      <MobileHeader />
+    </>
+  );
+};
+
+export default HomePage;
